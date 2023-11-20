100b194e40f3ebb28185ed2869594e133a09c69f
Iulik <inicula1989@gmail.com>
2023-11-20 19:10:12 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+const items = [
+  {
+      type: 'vegetable',
+      img: 'https://i5.walmartimages.com/seo/Fresh-Slicing-Tomato-Each_a1e8e44a-2b82-48ab-9c09-b68420f6954c.04f6e0e87807fc5457f57e3ec0770061.jpeg',
+      title: 'tomato',
+      price: 4.5,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem dolores, et ipsa maiores praesentium repudiandae.'
+  },
+  {
+      type: 'vegetable',
+      img: 'https://www.kroger.com/product/images/large/front/0000000004062',
+      title: 'cucumber',
+      price: 3,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem dolores, et ipsa maiores praesentium repudiandae.'
+  },
+  {
+      type: 'vegetable',
+      img: 'https://cdn.britannica.com/89/170689-131-D20F8F0A/Potatoes.jpg',
+      title: 'potato',
+      price: 2,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem dolores, et ipsa maiores praesentium repudiandae.'
+  },
+  {
+      type: 'vegetable',
+      img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Onion_on_White.JPG/1200px-Onion_on_White.JPG',
+      title: 'onion',
+      price: 1.5,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem dolores, et ipsa maiores praesentium repudiandae.'
+  },
+  {
+      type: 'vegetable',
+      img: 'https://green-connect.com.au/wp-content/uploads/2022/05/Cabbage2-scaled.jpg',
+      title: 'cabbage',
+      price: 0.75,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem dolores, et ipsa maiores praesentium repudiandae.'
+  },
+  {
+      type: 'vegetable',
+      img: 'https://domf5oio6qrcr.cloudfront.net/medialibrary/5390/h1218g16207258089583.jpg',
+      title: 'broccoli',
+      price: 5,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem dolores, et ipsa maiores praesentium repudiandae.'
+  },
+  {
+      type: 'fruits',
+      img: 'https://subzfresh.com/wp-content/uploads/2022/04/apple_158989157.jpg',
+      title: 'apple',
+      price: 3,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem dolores, et ipsa maiores praesentium repudiandae.'
+  },
+  {
+      type: 'fruits',
+      img: 'https://fruitboxco.com/cdn/shop/products/asset_2_1024x.jpg?v=1571839043',
+      title: 'banana',
+      price: 3.5,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem dolores, et ipsa maiores praesentium repudiandae.'
+  },
+  {
+      type: 'fruits',
+      img: 'https://familytreefarms.com/wp-content/uploads/2021/03/PeachWhite.jpg',
+      title: 'peach',
+      price: 2.5,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem dolores, et ipsa maiores praesentium repudiandae.'
+  },
+  {
+      type: 'fruits',
+      img: 'https://media6.ppl-media.com/mediafiles/blogs/VNO_Mandarin1_5cb26f68b7.jpg',
+      title: 'mandarin',
+      price: 6,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem dolores, et ipsa maiores praesentium repudiandae.'
+  },
+  {
+      type: 'fruits',
+      img: 'https://www.healthxchange.sg/sites/hexassets/Assets/food-nutrition/pineapple-health-benefits-and-ways-to-enjoy.jpg',
+      title: 'pineapple',
+      price: 10,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem dolores, et ipsa maiores praesentium repudiandae.'
+  },
+  {
+      type: 'meet',
+      img: 'https://www.licious.in/blog/wp-content/uploads/2022/03/Chicken-Curry-Cut-min-750x750.png',
+      title: 'chicken',
+      price: 7,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem dolores, et ipsa maiores praesentium repudiandae.'
+  },
+  {
+      type: 'meet',
+      img: 'https://cdn.britannica.com/68/143268-050-917048EA/Beef-loin.jpg',
+      title: 'beef',
+      price: 12,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem dolores, et ipsa maiores praesentium repudiandae.'
+  },
+  {
+      type: 'meet',
+      img: 'https://media.post.rvohealth.io/wp-content/uploads/2020/08/pork-1200x628-facebook-1200x628.jpg',
+      title: 'pork',
+      price: 7,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem dolores, et ipsa maiores praesentium repudiandae.'
+  },
+  {
+      type: 'meet',
+      img: 'https://www.onceuponachef.com/images/2018/02/pan-seared-salmon-.jpg',
+      title: 'salmon',
+      price: 15,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem dolores, et ipsa maiores praesentium repudiandae.'
+  }
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello      
     </div>
   );
 }
